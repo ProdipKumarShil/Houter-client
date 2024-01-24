ade89cecf8fee60ba3e78af128ede1b3ae482f0b
ProdipKumarShil <prodipkrishna01@gmail.com>
2024-01-24 11:18:13 +0600

    added sign in and sign up page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,24 @@
-
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="max-w-screen-xl mx-auto">
+      <div className="max-w-80 mx-auto space-y-5">
+        <p className="text-[24px] ">Sign In Here</p>
+        <input
+          className="border border-my-secondary p-3 rounded-lg w-full"
+          placeholder="Enter Your Email"
+          type="email"
+        />
+        <input
+          className="border border-my-secondary p-3 rounded-lg w-full"
+          placeholder="Enter Your Password"
+          type="password"
+        />
+        <button className=" bg-my-primary text-white py-3 rounded-lg w-full font-bold">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,40 @@
-
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="max-w-screen-xl mx-auto">
+      <div className="max-w-80 mx-auto space-y-5">
+        <p className="text-[24px] ">Sign Up Here</p>
+        <input
+          className="border border-my-secondary p-3 rounded-lg w-full"
+          placeholder="Enter Your Name"
+          type="text"
+        />
+        <input
+          className="border border-my-secondary p-3 rounded-lg w-full"
+          placeholder="Enter Your Email"
+          type="email"
+        />
+        <input
+          className="border border-my-secondary p-3 rounded-lg w-full"
+          placeholder="Enter Your Phone"
+          type="number"
+        />
+        <select className="border border-my-secondary p-3 rounded-lg w-full" name="role">
+          <option value='' >Select a role</option>
+          <option value="renter">Renter</option>
+          <option value="owner">Owner</option>
+          
+        </select>
+        <input
+          className="border border-my-secondary p-3 rounded-lg w-full"
+          placeholder="Enter Your Password"
+          type="password"
+        />
+        <button className=" bg-my-primary text-white py-3 rounded-lg w-full font-bold">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
