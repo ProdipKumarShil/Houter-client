05e0407f53a750f2b466d945c8c2334ecb375c70
ProdipKumarShil <prodipkrishna01@gmail.com>
2024-01-31 01:31:05 +0600

    fix animation

--- src/shared/Heading/HeadingCenter.jsx
+++ src/shared/Heading/HeadingCenter.jsx
@@ -6,15 +6,28 @@ gsap.registerPlugin(ScrollTrigger)
 
 const HeadingCenter = ({ header, title, triggerName }) => {
   const textRef = useRef(null);
-  const triggerRef = useRef(null);
+  const headerRef = useRef(null);
 
   useEffect(() => {
     const splitText = new SplitType(textRef.current);
+    const splitWord = new SplitType(headerRef.current)
+
+    gsap.from(splitWord.words, {
+      scrollTrigger: {
+        trigger: `.${triggerName}`,
+        markers: false
+      },
+      y: -100,
+      opacity: 0,
+      stagger: 0.2,
+      ease: "back",
+      duration: 1
+    })
 
     gsap.from(splitText.chars, {
       scrollTrigger: {
         trigger: `.${triggerName}`,
-        markers: true
+        markers: false
       },
       opacity: 0,
       duration: 0.5,
@@ -25,10 +38,10 @@ const HeadingCenter = ({ header, title, triggerName }) => {
     });
   }, [triggerName]);
   return (
-    <div ref={triggerRef} className="textAnim">
+    <div className="textAnim">
       <p className="text-center mb-3">
         <div className="w-[32px] mx-auto border border-[#F59E0B] mb-2"></div>{" "}
-        <p className="font-medium text-[#F59E0B]">{header}</p>
+        <p ref={headerRef} className={`${triggerName} overflow-hidden font-medium text-[#F59E0B]`}>{header}</p>
       </p>
       <div className="overflow-hidden ">
         <p
